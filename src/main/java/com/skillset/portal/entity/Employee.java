f3257a63f8b5bab61a5d079421308b7f52454e77
Balu <balu.s>
2026-07-02 17:47:00 +0530

    First commit

---
+++ src/main/java/com/skillset/portal/entity/Employee.java
@@ -0,0 +1,35 @@
+package com.skillset.portal.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
+
+@Entity
+
+@Table(name = "employee")
+
+@Data
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+
+public class Employee {
+
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer employeeId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+}
